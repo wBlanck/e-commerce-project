78e6fcd9cc69eef4db2ebc0d6d4054bb6984d815
wblanck - laptop <blanck.william@gmail.com>
2022-01-17 04:40:21 +0100

    broken homepage into components, added styles

---
+++ src/pages/homepage/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./HomePage.styles.scss";
+import Directory from "../../components/directory/Directory.component.jsx";
+
+export const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
